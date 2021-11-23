46ddee808c693083fa97d23f5b0694856002f67d
tomasz <t.limisiewicz@gmail.com>
2021-11-23 14:13:44 +0100

    Seemingly working probing algorithm

--- src/network.py
+++ src/network.py
@@ -26,20 +26,19 @@ class Network():
             self.probe_rank = args.probe_rank
             self.model_dim = constants.MODEL_DIMS[args.model]
             self.languages = args.languages
-            self.tasks = [task for task in args.tasks if "distance" in task]
+            self.tasks = args.tasks
 
             self._orthogonal_reg = args.ortho
-            self.ml_probe = args.ml_probe
             
             self.average_layers = (args.layer_index == -1)
             
-            self.LanguageMaps = {lang: tf.Variable(tf.initializers.Identity(gain=1.0)((self.model_dim, self.probe_rank)),
-                                                   trainable=self.ml_probe, name='{}_map'.format(lang))
+            self.OrthogonalTransformations = {lang: tf.Variable(tf.initializers.Identity(gain=1.0)((self.model_dim, self.probe_rank)),
+                                                   trainable=True, name='{}_map'.format(lang))
                                  for lang in self.languages}
 
             if self._orthogonal_reg:
                 # if orthogonalization is used multilingual probe is only diagonal scaling
-                self.DistanceProbe = {task: tf.Variable(tf.random_uniform_initializer(minval=-0.5, maxval=0.5, seed=args.seed)
+                self.BinaryProbe = {task: tf.Variable(tf.random_uniform_initializer(minval=-0.5, maxval=0.5, seed=args.seed)
                                                         ((1, self.probe_rank,)),
                                                         trainable=True, name=f'{task}_probe', dtype=tf.float32)
                                       for task in self.tasks}
@@ -84,22 +83,17 @@ class Network():
         def decrease_lr(self, decay_factor):
             self._lr *= decay_factor
             self._optimizer.learning_rate.assign(self._lr)
-            # zeroing optimizer weights in`no ml` setting to reproduce Hewitt's results
-            if not self.ml_probe:
-                reset_variables = [np.zeros_like(var.numpy()) for var in self._optimizer.variables()]
-                self._optimizer.set_weights(reset_variables)
 
         @tf.function
         def get_projections(self, embeddings, max_token_len, language, task):
             """ Computes projections after Orthogonal Transformation, and after Dimension Scaling"""
             embeddings = embeddings[:, :max_token_len, :]
-            orthogonal_projections = None
-            if self.ml_probe:
-                orthogonal_projections = embeddings @ self.LanguageMaps[language]
-            if (self._orthogonal_reg and self.ml_probe):
-                projections = orthogonal_projections * self.DistanceProbe[task]
+
+            orthogonal_projections = embeddings @ self.OrthogonalTransformations[language]
+            if self._orthogonal_reg:
+                projections = orthogonal_projections * self.BinaryProbe[task]
             else:
-                projections = embeddings @ self.DistanceProbe[task]
+                projections = embeddings @ self.BinaryProbe[task]
     
             return orthogonal_projections, projections
 
@@ -116,48 +110,54 @@ class Network():
             _, projections = self.get_projections(embeddings, max_token_len, language, task)
             if embeddings_gate is not None:
                 projections = projections * embeddings_gate
-    
-            projections = tf.expand_dims(projections, 1)  # shape [batch, 1, seq_len, emb_dim]
-            transposed_projections = tf.transpose(projections, perm=(0, 2, 1, 3))  # shape [batch, seq_len, 1, emb_dim]
-            diffs = projections - transposed_projections  # shape [batch, seq_len, seq_len, emb_dim]
-            squared_diffs = tf.reduce_sum(tf.math.square(diffs), axis=-1) # shape [batch, seq_len, seq_len]
+
+            projections = tf.expand_dims(projections, 1)  # shape [batch, 1, emb_dim]
+            transposed_projections = tf.transpose(projections, perm=(1, 0, 2))  # shape [1, batch, emb_dim]
+            diffs = projections - transposed_projections  # shape [batch, batch, emb_dim]
+            squared_diffs = tf.reduce_sum(tf.math.square(diffs), axis=-1) # shape [batch, batch]
             return squared_diffs
 
         @tf.function
         def _loss(self, predicted_distances, gold_distances, mask, token_lens):
-            sentence_loss = tf.reduce_sum(tf.abs(predicted_distances - gold_distances) * mask, axis=[1,2]) / \
-                            tf.clip_by_value(tf.reduce_sum(mask, axis=[1,2]), 1., constants.MAX_TOKENS ** 2.)
-            return tf.reduce_mean(sentence_loss)
+            loss = tf.reduce_sum(tf.abs(predicted_distances - gold_distances) * mask) / \
+                            tf.clip_by_value(tf.reduce_sum(mask), 1., constants.MAX_TOKENS ** 2.)
+            return loss
 
         def train_factory(self, language, task):
             # separate train function is needed to avoid variable creation on non-first call
             # see: https://github.com/tensorflow/tensorflow/issues/27120
             @tf.function(experimental_relax_shapes=True)
-            def train_on_batch(target, mask, token_len, embeddings, l1_lambda=0.0, mst_s=None):
+            def train_on_batch(bias, information, is_object, token_len, embeddings, l1_lambda=0.0, mst_s=None):
         
                 with tf.GradientTape() as tape:
                     max_token_len = tf.reduce_max(token_len)
-                    target = target[:,:max_token_len,:max_token_len]
-                    mask = mask[:,:max_token_len,:max_token_len]
+
+                    if 'bias' in task:
+                        feature_vector = bias # [batch]
+                    else:
+                        feature_vector = information
+
+                    feature_vector = tf.expand_dims(feature_vector, 1) # shape [batch, 1]
+                    transposed_feature_vector = tf.transpose(feature_vector) # shape [1, batch]
+
+                    target = tf.math.logical_xor(feature_vector, transposed_feature_vector)
+                    target = tf.cast(target, dtype=tf.float32)
+                    mask = tf.ones_like(target, dtype=tf.float32)
+                    mask = tf.linalg.set_diag(mask, tf.zeros(mask.shape[0:-1]))
+
                     predicted_distances = self._forward(embeddings, max_token_len, language, task)
-                    # Here MST loss can be used (proposed by Maudslay et al. https://arxiv.org/pdf/2005.01641.pdf)
-                    #if task == 'dep_distance':
-                    #    loss = self._loss_mst(predicted_distances, target, mask, token_len, mst_s)
-                    #else:
+
                     loss = self._loss(predicted_distances, target, mask, token_len)
-                    if self._orthogonal_reg and self.ml_probe:
-                        ortho_penalty = self.ortho_reguralization(self.LanguageMaps[language])
+                    if self._orthogonal_reg:
+                        ortho_penalty = self.ortho_reguralization(self.OrthogonalTransformations[language])
                         loss += self._orthogonal_reg * ortho_penalty
-                    if self._l1_reg and self.ml_probe:
-                        probe_l1_penalty = tf.norm(self.DistanceProbe[task], ord=1)
+                    if self._l1_reg:
+                        probe_l1_penalty = tf.norm(self.BinaryProbe[task], ord=1)
                         loss += l1_lambda * probe_l1_penalty
         
-                if self.ml_probe and language in self.skipped_languages and 'dep_' in task:
-                    variables = [self.LanguageMaps[language]]
-                elif self.ml_probe:
-                    variables = [self.DistanceProbe[task], self.LanguageMaps[language]]
-                else:
-                    variables = [self.DistanceProbe[task]]
+
+                variables = [self.BinaryProbe[task], self.OrthogonalTransformations[language]]
+
         
                 if self.average_layers:
                     variables.append(self.LayerWeights[f'lw_{task}'])
@@ -172,17 +172,15 @@ class Network():
                 with self._writer.as_default(), tf.summary.record_if(self._optimizer.iterations % 20 == 0
                                                                            or self._optimizer.iterations == 1):
                     tf.summary.scalar("train/batch_loss_{}".format(language), loss)
-                    if not(language in self.skipped_languages and 'dep_' in task):
-                        tf.summary.scalar("train/probe_gradient_norm", gradient_norms[0])
-                    else:
-                        tf.summary.scalar("train/{}_map_gradient_norm".format(language), gradient_norms[0])
-                    if self._orthogonal_reg and self.ml_probe:
+                    tf.summary.scalar("train/probe_gradient_norm", gradient_norms[0])
+                    tf.summary.scalar("train/{}_map_gradient_norm".format(language), gradient_norms[1])
+
+                    if self._orthogonal_reg:
                         tf.summary.scalar("train/{}_nonorthogonality_penalty".format(language), ortho_penalty)
-                    if self._l1_reg and self.ml_probe:
+                    if self._l1_reg:
                         tf.summary.scalar("train/probe_l1_penalty", probe_l1_penalty)
                         tf.summary.scalar("train/l1_lambda", l1_lambda)
-                    if self.ml_probe and not(language in self.skipped_languages and 'dep_' in task):
-                        tf.summary.scalar("train/{}_map_gradient_norm".format(language), gradient_norms[1])
+                    tf.summary.scalar("train/{}_map_gradient_norm".format(language), gradient_norms[1])
         
                 return loss
             return train_on_batch
@@ -213,18 +211,18 @@ class Network():
 
         # Checkpoint managment:
         self.ckpt = tf.train.Checkpoint(optimizer=self.probe._optimizer,
-                                        **self.probe.LanguageMaps,
+                                        **self.probe.OrthogonalTransformations,
                                         **self.probe.LayerWeights,
-                                        **self.probe.DistanceProbe)
+                                        **self.probe.BinaryProbe)
         self.checkpoint_manager = tf.train.CheckpointManager(self.ckpt, os.path.join(args.out_dir, 'params'),
                                                              max_to_keep=1)
 
     @staticmethod
-    def decode(serialized_example, task, layer_idx, model):
-        features_to_decode = {'num_tokens': tf.io.FixedLenFeature([], tf.int64),
-                              'index': tf.io.FixedLenFeature([], tf.int64),
-                              f'target_{task}': tf.io.FixedLenFeature([], tf.string),
-                              f'mask_{task}': tf.io.FixedLenFeature([], tf.string)
+    def decode(serialized_example, layer_idx, model):
+        features_to_decode = {"index": tf.io.FixedLenFeature([], tf.int64),
+                              'bias': tf.io.FixedLenFeature([], tf.bool),
+                              'information': tf.io.FixedLenFeature([], tf.bool),
+                              'is_object': tf.io.FixedLenFeature([], tf.bool)
                               }
         if layer_idx == -1:
             features_to_decode.update({f'layer_{idx}': tf.io.FixedLenFeature([], tf.string)
@@ -237,8 +235,9 @@ class Network():
             features= features_to_decode)
     
         index = tf.cast(x["index"], dtype=tf.int64)
-        target = tf.io.parse_tensor(x[f"target_{task}"], out_type=tf.float32)
-        mask = tf.io.parse_tensor(x[f"mask_{task}"], out_type=tf.float32)
+        bias = tf.cast(x["bias"], dtype=tf.bool)
+        information = tf.cast(x["information"], dtype=tf.bool)
+        is_object = tf.cast(x["is_object"], dtype=tf.bool)
         num_tokens = tf.cast(x["num_tokens"], dtype=tf.int64)
         if layer_idx == -1:
             embeddings = [tf.io.parse_tensor(x[f"layer_{idx}"], out_type=tf.float32)
@@ -248,7 +247,7 @@ class Network():
         else:
             embeddings = tf.io.parse_tensor(x[f"layer_{layer_idx}"], out_type=tf.float32)
     
-        return index, target, mask, num_tokens, embeddings
+        return index, bias, information, is_object, num_tokens, embeddings
 
     @staticmethod
     def data_pipeline(tf_data, languages, tasks, args, mode='train'):
@@ -262,19 +261,11 @@ class Network():
                 for task in tasks:
                     if task not in tf_data[lang]:
                         raise ValueError(f"Task: {task} not found in the data set")
-                    elif lang in args.zs_dep_languages and 'dep_' in task:
-                        continue
                 
                     data = tf_data[lang][task]
                 
                     data = data.map(partial(Network.decode, task=task, layer_idx=args.layer_index, model=args.model),
                                     num_parallel_calls=tf.data.experimental.AUTOTUNE)
-                    if lang in args.fs_dep_languages and 'dep_' in task and mode == 'train':
-                        data = data.shuffle(constants.SHUFFLE_SIZE, args.seed)
-                        data = data.take(args.fewshot_size)
-                    elif mode == 'train' and args.subsample_train:
-                        data = data.shuffle(constants.SHUFFLE_SIZE, args.seed)
-                        data = data.take(args.subsample_train)
                     if args.layer_index >= 0:
                         data = data.cache()
                     if mode == 'train':
@@ -308,9 +299,9 @@ class Network():
                 lang = lang.numpy().decode()
                 task = task.numpy().decode()
             
-                _, batch_target, batch_mask, batch_num_tokens, batch_embeddings = batch
+                _, batch_bias, batch_information, batch_is_object, batch_num_tokens, batch_embeddings = batch
 
-            batch_loss = self.probe._train_fns[lang][task](batch_target, batch_mask, batch_num_tokens, batch_embeddings, l1_lambda)
+            batch_loss = self.probe._train_fns[lang][task](batch_bias, batch_information, batch_is_object, batch_num_tokens, batch_embeddings, l1_lambda)
         
             progressbar.set_description(f"Training, batch loss: {batch_loss:.4f}, memory: {resource.getrusage(resource.RUSAGE_SELF).ru_maxrss /1024 / 1024:.2f}")
         
@@ -330,7 +321,7 @@ class Network():
         
             if self.probe._l1_reg and not l1_lambda:
                 sum_ortho_penalty = 0.
-                for orthogonal_matrix in self.probe.LanguageMaps.values():
+                for orthogonal_matrix in self.probe.OrthogonalTransformations.values():
                     sum_ortho_penalty += self.probe.ortho_reguralization(orthogonal_matrix)
             
                 if sum_ortho_penalty <= self.PENALTY_THRESHOLD:
---
+++ src/probe.py
@@ -0,0 +1,65 @@
+import os
+import argparse
+import json
+
+from data_wrappers.tfrecord_wrapper import TFRecordReader
+from network import Network
+
+import constants
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	#
+	parser.add_argument("parent_dir", type=str, default="../experiments", help="Parent experiment directory")
+	parser.add_argument("data_dir", type=str, default='../resources/tf_data',
+	                    help="Directory where tfrecord files are stored")
+	parser.add_argument("--json-data", type=str, default=None, help="JSON with conllu and languages for training")
+
+	parser.add_argument("--languages", nargs='*', default=['en'], type=str,
+	                    help="Languages to probe.")
+	parser.add_argument("--tasks", nargs='*', type=str,
+	                    help="Probing tasks (gender information or bias)")
+
+	# Probe arguments
+	parser.add_argument("--probe-rank", default=None, type=int, help="Rank of the probe")
+	parser.add_argument("--layer-index", default=6, type=int, help="Index of BERT's layer to probe."
+	                                                               "If -1 all layers embeddings are averaged")
+	# Train arguments
+	parser.add_argument("--seed", default=42, type=int, help="Seed for variable initialisation")
+	parser.add_argument("--batch-size", default=12, type=int, help="Batch size")
+	parser.add_argument("--epochs", default=25, type=int, help="Maximal number of training epochs")
+	parser.add_argument("--learning-rate", default=0.001, type=float, help="Initial learning rate")
+	parser.add_argument("--ortho", default=None, type=float,
+	                    help="Orthogonality reguralization (SRIP) for language map matrices.")
+	parser.add_argument("--l1", default=None, type=float, help="L1 reguralization of the weights.")
+	parser.add_argument("--clip-norm", default=None, type=float, help="Clip gradient norm to this value")
+
+	parser.add_argument("--subsample-train", default=None, type=int,
+	                    help="Size of subsample taken from a training set.")
+
+	# Specify Bert Model
+	parser.add_argument("--model",
+	                    default=f"bert-{constants.SIZE_BASE}-{constants.LANGUAGE_MULTILINGUAL}-{constants.CASING_CASED}",
+	                    help="Transformer model name (see: https://huggingface.co/transformers/pretrained_models.html)")
+
+	args = parser.parse_args()
+
+	if not args.probe_rank:
+		args.probe_rank = constants.MODEL_DIMS[args.model]
+
+	do_lower_case = (constants.CASING_UNCASED in args.model)
+
+	if args.seed == 42:
+		experiment_name = f"task_{'_'.join(args.tasks)}-layer_{args.layer_index}-trainl_{'_'.join(args.languages)}"
+	else:
+		experiment_name = f"task_{'_'.join(args.tasks)}-layer_{args.layer_index}-trainl_{'_'.join(args.languages)}-seed_{args.seed}"
+	args.out_dir = os.path.join(args.parent_dir, experiment_name)
+	if not os.path.exists(args.out_dir):
+		os.mkdir(args.out_dir)
+
+	tf_reader = TFRecordReader(args.data_dir, args.model)
+	tf_reader.read(args.tasks, args.languages)
+
+	network = Network(args)
+
+	network.train(tf_reader, args)
